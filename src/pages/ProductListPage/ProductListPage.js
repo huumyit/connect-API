44651af6c3b4a0f5e4cc9ed79ca1ac8eaaccf396
TonyHuynh <huumyit@gmail.com>
2017-12-13 16:37:58 +0700

    Config axios API

--- src/pages/ProductListPage/ProductListPage.js
+++ src/pages/ProductListPage/ProductListPage.js
@@ -2,9 +2,41 @@ import React, {Component} from 'react';
 import ProductList from './../../components/ProductList/ProductList';
 import ProductItem from './../../components/ProductItem/ProductItem';
 import { connect } from 'react-redux';
+// import axios from 'axios';
+import callApi from './../../utils/apiCaller';
 
 class ProductListPage extends Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      products: []
+    };
+  }
+
+  componentDidMount() {
+    callApi('products', 'GET', null).then(response => {
+      this.setState({
+        products: response.data
+      });
+    })
+
+    // Send a GET request
+    // axios({
+    //   method: 'GET',
+    //   url: 'http://localhost:3000/products',
+    //   data: null
+    // })
+    // .then(response => {
+    //   this.setState({
+    //     products: response.data
+    //   });
+    // })
+    // .catch(error => {
+    //   console.log(error);
+    // });
+  }
+  
   showProducts(products) {
     var result = null;
     if (products.length > 0) {
@@ -22,7 +54,10 @@ class ProductListPage extends Component {
   }
 
   render() {
-    var { products } = this.props;
+    // var { products } = this.props;
+    var { products } = this.state;
+    console.log(products);
+
     return (
       <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
         <button type="button" className="btn btn-large btn-info mb-10">Add product</button>
